a4e42b0309871abd74927f9268e9bfe6c57c49f4
Bui Quoc Tuan <tuanbuinov09@gmail.com>
2024-04-06 13:47:47 +0700

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -45,9 +45,8 @@ let postWebhook = (req, res) => {
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
 
-            const adminsPSIDs = await getAdminsPSIDs(process.env.FB_PAGE_ID);
-
-            console.log("adminsPSIDs", adminsPSIDs)
+            // const adminsPSIDs = await getAdminsPSIDs(process.env.FB_PAGE_ID);
+            // console.log("adminsPSIDs", adminsPSIDs)
 
             //TODO:
             //if page is sending to user, bot deactivate for 5 min for this user
@@ -101,6 +100,53 @@ let getWebhook = (req, res) => {
     }
 };
 
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+    let response;
+
+    // Check if the message contains text
+    if (received_message.text) {
+
+        // Create the payload for a basic text message
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    } else if (received_message.attachments) {
+
+        // Gets the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
+
+    }
+
+    // Sends the response message
+    callSendAPI(sender_psid, response);
+}
+
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
     let response;
@@ -125,7 +171,8 @@ function callSendAPI(sender_psid, response) {
         "recipient": {
             "id": sender_psid
         },
-        "message": { "text": response }
+        // "message": { "text": response }
+        "message": response
     };
 
     // Send the HTTP request to the Messenger Platform
@@ -147,43 +194,43 @@ function firstTrait(nlp, name) {
     return nlp && nlp.entities && nlp.traits[name] && nlp.traits[name][0];
 }
 
-function handleMessage(sender_psid, message) {
-    //handle message for react, like press like button
-    // id like button: sticker_id 369239263222822
+// function handleMessage(sender_psid, message) {
+//     //handle message for react, like press like button
+//     // id like button: sticker_id 369239263222822
 
-    if (message && message.attachments && message.attachments[0].payload) {
-        callSendAPI(sender_psid, "Thank you for using our page !!!");
-        callSendAPIWithTemplate(sender_psid);
-        return;
-    }
+//     if (message && message.attachments && message.attachments[0].payload) {
+//         callSendAPI(sender_psid, "Thank you for using our page !!!");
+//         callSendAPIWithTemplate(sender_psid);
+//         return;
+//     }
 
-    let entitiesArr = ["wit$greetings", "wit$thanks", "wit$bye"];
-    let entityChosen = "";
-    entitiesArr.forEach((name) => {
-        let entity = firstTrait(message.nlp, name);
-        if (entity && entity.confidence > 0.8) {
-            entityChosen = name;
-        }
-    });
+//     let entitiesArr = ["wit$greetings", "wit$thanks", "wit$bye"];
+//     let entityChosen = "";
+//     entitiesArr.forEach((name) => {
+//         let entity = firstTrait(message.nlp, name);
+//         if (entity && entity.confidence > 0.8) {
+//             entityChosen = name;
+//         }
+//     });
 
-    if (entityChosen === "") {
-        //default
-        callSendAPI(sender_psid, `The bot is needed more training, try to say "thanks a lot" or "hi" to the bot`);
-    } else {
-        if (entityChosen === "wit$greetings") {
-            //send greetings message
-            callSendAPI(sender_psid, 'Hi there! This bot is a bot!');
-        }
-        if (entityChosen === "wit$thanks") {
-            //send thanks message
-            callSendAPI(sender_psid, `You 're welcome!`);
-        }
-        if (entityChosen === "wit$bye") {
-            //send bye message
-            callSendAPI(sender_psid, 'bye-bye!');
-        }
-    }
-}
+//     if (entityChosen === "") {
+//         //default
+//         callSendAPI(sender_psid, `The bot is needed more training, try to say "thanks a lot" or "hi" to the bot`);
+//     } else {
+//         if (entityChosen === "wit$greetings") {
+//             //send greetings message
+//             callSendAPI(sender_psid, 'Hi there! This bot is a bot!');
+//         }
+//         if (entityChosen === "wit$thanks") {
+//             //send thanks message
+//             callSendAPI(sender_psid, `You 're welcome!`);
+//         }
+//         if (entityChosen === "wit$bye") {
+//             //send bye message
+//             callSendAPI(sender_psid, 'bye-bye!');
+//         }
+//     }
+// }
 
 let callSendAPIWithTemplate = (sender_psid) => {
     // document fb message template
